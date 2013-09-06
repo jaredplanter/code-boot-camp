1c084dc96c689aafd45ee12167dbf752c9ae9f29
Jared Planter <jared.planter@activenetwork.com>
2013-09-06 07:20:32 -0700

    initializing repo

---
+++ asgn2.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool is_vowel( char arg )
+{ 
+  int i;
+  if(arg < 91){
+    arg = arg + 32;
+  }
+  char vowel[] = {'a','e','i','o','u'};
+  for (i = 0; i < 10; ++i){
+    if (arg == vowel[i])
+    {
+        return 1;
+    }
+  }
+
+  return 0;
+}
+
+int main( int argc, char *argv[] )
+{
+  char word[] = {};
+  char *quit = "exit";
+  
+  while( strncmp(word, quit, 4) )
+  {
+    puts( "Enter a message:" );
+    
+    fgets( word, 10, stdin  );
+    for( int i = 0; i <  strlen(word); i++ )
+    {
+      if( is_vowel( word[i]) )
+      {
+        printf( "Vowel: %c\n", word[i] );
+      }
+      else{
+        printf( "%c\n", word[i] );
+      }
+    }
+  }
+  
+  return 0;
+}
